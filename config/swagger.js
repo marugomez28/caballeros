0f394d9aaa23dd777cb9db4030afb940ab67925c
MgtSolutions-Cop <mgtsolutios@gmail.com>
2025-10-30 23:47:29 -0500

    Cambios en la documentacion

--- config/swagger.js
+++ config/swagger.js
@@ -11,7 +11,7 @@ const options = {
     },
     servers: [
       {
-        url: "http://localhost:4000",
+        url: "caballeros-production.up.railway.app",
         description: "Servidor local",
       },
     ],

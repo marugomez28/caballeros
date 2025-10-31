5a0ec4a81d19574e3ea6d6c623a0fea1033332f9
MgtSolutions-Cop <mgtsolutios@gmail.com>
2025-10-30 23:33:15 -0500

    Cambio nombre de archivo

--- Server.js
+++ Server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 mongoose
   .connect(process.env.MONGODB_URI)
-  .then(() => console.log("MongoDB conectado"))
+  .then(() => console.log("MongoAtlas conectado"))
   .catch((err) => console.error(err));
 
 app.use("/api/caballeros", caballeros);
